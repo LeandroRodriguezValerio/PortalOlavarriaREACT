bac9baf7ec874802b0e4ce84cc25b9da1ab722d7
Leandro Contreras <leandro_cont_95@hotmail.com>
2025-10-20 19:35:48 -0300

    creacion del archivo registro.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import registro from './components/registro'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -32,8 +33,11 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      { <div>
+        <registro />
+      </div> }
     </>
-  )
+  );
 }
 
 export default App
